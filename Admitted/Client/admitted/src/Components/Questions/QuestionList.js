943e97974db06971bea85185f0d29f02de1b5e60
guacajoely <guacajoely@gmail.com>
2023-08-08 15:01:25 -0400

    all answered questions are now rendered before unanswered

--- Admitted/Client/admitted/src/Components/Questions/QuestionList.js
+++ Admitted/Client/admitted/src/Components/Questions/QuestionList.js
@@ -26,63 +26,107 @@ export const QuestionList = ({ admissionId }) => {
     };
 
     return (
-        
+
         <div className="questions-section">
             <h1>Questions <Button className="btn-sm m-1" color="primary" tag={Link} to={`/questions/create/${admissionId}`}>Add a Question</Button></h1>
 
             {questionList.length > 0 ?
 
-            <table className="questions-table">
-                <thead>
-                    <tr>
-                        <th style={{maxWidth: "10%"}}>Date</th>
-                        <th style={{maxWidth: "20%"}}>Question</th>
-                        <th style={{maxWidth: "10%"}}>Date</th>
-                        <th style={{maxWidth: "20%"}}>Response</th>
-                        <th style={{width: "150px"}} className="button-column"></th>
-                    </tr>
-                </thead>
+                <table className="questions-table">
+                    <thead>
+                        <tr>
+                            <th style={{ maxWidth: "10%" }}>Date</th>
+                            <th style={{ maxWidth: "20%" }}>Question</th>
+                            <th style={{ maxWidth: "10%" }}>Date</th>
+                            <th style={{ maxWidth: "20%" }}>Response</th>
+                            <th style={{ width: "150px" }} className="button-column"></th>
+                        </tr>
+                    </thead>
 
-                <tbody>
+                    <tbody>
 
-                    {questionList.map((question) => {
+                        {/* MAP ONLY QUESTIONS WITH ANSWERS FIRST */}
 
-                        const formattedQuestionDate = new Date(question.questionDateTime).toLocaleString(undefined, {
-                            month:  'short',
-                            day:    'numeric',
-                            hour:   '2-digit',
-                            minute: '2-digit',
-                        });
+                        {questionList.map((question) => {
 
-                        const formattedAnswerDate = new Date(question.answerDateTime).toLocaleString(undefined, {
-                            month:  'short',
-                            day:    'numeric',
-                            hour:   '2-digit',
-                            minute: '2-digit',
-                        });
+                            const formattedQuestionDate = new Date(question.questionDateTime).toLocaleString(undefined, {
+                                month: 'short',
+                                day: 'numeric',
+                                hour: '2-digit',
+                                minute: '2-digit',
+                            });
 
-                        return (
-                            <tr key={question.id}>
-                                <td>{formattedQuestionDate}</td>
-                                <td>{question.questionText}</td>
-                                <td>{question.answerDateTime ? formattedAnswerDate : "N/A"}</td>
-                                <td>{question.answerText ? question.answerText : "NO RESPONSE RECORDED"}</td>
-                                
-                                <td className="button-column">
-                                    <Button className="btn-sm m-1" tag={Link} to={`/questions/edit/${question.id}`} color="secondary">Edit</Button>
-                                    <Button id={`question--${question.id}`} className="btn-sm m-1" color="danger" tag={Link} onClick={handleDeleteButton}>Delete</Button>
-                                </td>
-                            </tr>
-                        )
-                    })}
+                            const formattedAnswerDate = new Date(question.answerDateTime).toLocaleString(undefined, {
+                                month: 'short',
+                                day: 'numeric',
+                                hour: '2-digit',
+                                minute: '2-digit',
+                            });
 
-                </tbody>
-            </table>
+                            if (question.answerText) {
+                                return (
+
+                                    <tr key={question.id}>
+                                        <td>{formattedQuestionDate}</td>
+                                        <td>{question.questionText}</td>
+                                        <td>{question.answerDateTime ? formattedAnswerDate : "N/A"}</td>
+                                        <td>{question.answerText ? question.answerText : "NO RESPONSE RECORDED"}</td>
+
+                                        <td className="button-column">
+                                            <Button className="btn-sm m-1" tag={Link} to={`/questions/edit/${question.id}`} color="secondary">Edit</Button>
+                                            <Button id={`question--${question.id}`} className="btn-sm m-1" color="danger" tag={Link} onClick={handleDeleteButton}>Delete</Button>
+                                        </td>
+                                    </tr>
+                                )
+                            }
+                        })}
 
 
-            :<></>
+                        {/* THEN MAP QUESTIONS WITHOUT ANSWERS */}
+
+                        {questionList.map((question) => {
+
+                            const formattedQuestionDate = new Date(question.questionDateTime).toLocaleString(undefined, {
+                                month: 'short',
+                                day: 'numeric',
+                                hour: '2-digit',
+                                minute: '2-digit',
+                            });
+
+                            const formattedAnswerDate = new Date(question.answerDateTime).toLocaleString(undefined, {
+                                month: 'short',
+                                day: 'numeric',
+                                hour: '2-digit',
+                                minute: '2-digit',
+                            });
+
+                            if (!question.answerText) {
+                                return (
+
+                                    <tr key={question.id}>
+                                        <td>{formattedQuestionDate}</td>
+                                        <td>{question.questionText}</td>
+                                        <td>{question.answerDateTime ? formattedAnswerDate : "N/A"}</td>
+                                        <td>{question.answerText ? question.answerText : "NO RESPONSE RECORDED"}</td>
+
+                                        <td className="button-column">
+                                            <Button className="btn-sm m-1" tag={Link} to={`/questions/edit/${question.id}`} color="secondary">Edit</Button>
+                                            <Button id={`question--${question.id}`} className="btn-sm m-1" color="danger" tag={Link} onClick={handleDeleteButton}>Delete</Button>
+                                        </td>
+                                    </tr>
+                                )
+                            }
+                        })}
+
+
+
+                    </tbody>
+                </table>
+
+
+                : <></>
             }
-            
+
         </div>
     )
 }
